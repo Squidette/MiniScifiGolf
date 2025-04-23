f4450180358c6c2f78dc53897206e05b56e7152a
Squidette <qfkrwldnjs@naver.com>
2025-04-23 17:19:15 +0900

    initial commit

---
+++ Source/MiniScifiGolf/Public/SimulationGameModeBase.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "SimulationGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class MINISCIFIGOLF_API ASimulationGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
