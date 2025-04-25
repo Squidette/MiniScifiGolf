79055f8e08402f6261a3deb802a67eb452c51684
Squidette <qfkrwldnjs@naver.com>
2025-04-25 18:14:35 +0900

    add magnus effect

--- Source/MiniScifiGolf/Private/GolfBallBase.cpp
+++ Source/MiniScifiGolf/Private/GolfBallBase.cpp
@@ -13,22 +13,22 @@ AGolfBallBase::AGolfBallBase()
  	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
 	PrimaryActorTick.bCanEverTick = true;
 
-	if (!RootComponent)
-	{
-		RootComponent = CreateDefaultSubobject<USceneComponent>(TEXT("ProjectileSceneComponent"));
-	}
-		
 	if (!SphereComp)
 	{
 		SphereComp = CreateDefaultSubobject<USphereComponent>(TEXT("Collider"));
-		SphereComp->InitSphereRadius(GOLFBALLBASE_RADIUS);
 		RootComponent = SphereComp;
+		SphereComp->SetCollisionProfileName(TEXT("BlockAllDynamic"));
+		SphereComp->SetSimulatePhysics(true);
+		SphereComp->InitSphereRadius(GOLFBALLBASE_RADIUS);
+		SphereComp->SetUseCCD(true);
 	}
 
 	if (!StaticMeshComp)
 	{
 		StaticMeshComp = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("StaticMesh"));
 		StaticMeshComp->SetupAttachment(SphereComp);
+		StaticMeshComp->SetCollisionProfileName(TEXT("NoCollision"));
+		
 		float meshScale = GOLFBALLBASE_RADIUS / 50.0f;
 		StaticMeshComp->SetRelativeScale3D(FVector(meshScale, meshScale, meshScale));
 	}
@@ -38,7 +38,6 @@ AGolfBallBase::AGolfBallBase()
 	{
 		StaticMeshComp->SetStaticMesh(MeshAsset.Object);
 	}
-
 }
 
 // Called when the game starts or when spawned
@@ -46,27 +45,59 @@ void AGolfBallBase::BeginPlay()
 {
 	Super::BeginPlay();
 
-	
-	
+	InitialTransform = GetActorTransform();
+
+	LastLocation = GetActorLocation();
+	GetWorldTimerManager().SetTimer(VisualizationHandle, this, &AGolfBallBase::Visualize, VisualizationCycle, true);
 }
 
 // Called every frame
 void AGolfBallBase::Tick(float DeltaTime)
 {
 	Super::Tick(DeltaTime);
-	
+
+	// 시뮬레이션용
 	if (APlayerController* PC = GetWorld()->GetFirstPlayerController())
 	{
-		if (PC->WasInputKeyJustPressed(EKeys::Q))
+		if (!simulated && PC->WasInputKeyJustPressed(EKeys::S))
 		{
-			UE_LOG(LogTemp, Warning, TEXT("Q was just pressed!"));
+			Shot();
+			simulated = true;
+		}
+
+		if (PC->WasInputKeyJustPressed(EKeys::R))
+		{
+			ResetGolfBall();
+			UE_LOG(LogTemp, Warning, TEXT("Reset"));
+			simulated = false;
 		}
 	}
 
+	// MAGNUS
+	FVector MagnusForce = FVector::CrossProduct(SphereComp->GetPhysicsLinearVelocity(), SphereComp->GetPhysicsAngularVelocityInDegrees() * MagnusScalar);
+	SphereComp->AddForce(MagnusForce);
 	
 }
 
 void AGolfBallBase::Shot()
 {
-	
+	SphereComp->AddImpulse(ImpulseAmount, TEXT("None"), false);
+	SphereComp->AddTorqueInRadians(TorqueAmount);
+}
+
+void AGolfBallBase::Visualize()
+{
+	if (LastLocation != GetActorLocation())
+	{
+		DrawDebugLine(GetWorld(), LastLocation, GetActorLocation(), FColor::Red, true);
+	}
+	LastLocation = GetActorLocation();
+}
+
+void AGolfBallBase::ResetGolfBall()
+{
+	SetActorTransform(InitialTransform);
+	//SphereComp->ComponentVelocity = FVector(0, 0, 0);
+	SphereComp->SetPhysicsLinearVelocity(FVector(0, 0, 0));
+	SphereComp->SetPhysicsAngularVelocityInRadians(FVector(0, 0, 0));
 }
--- Source/MiniScifiGolf/Public/GolfBallBase.h
+++ Source/MiniScifiGolf/Public/GolfBallBase.h
@@ -27,15 +27,36 @@ public:
 	UPROPERTY(EditAnywhere)
 	class USphereComponent* SphereComp;
 	
-	UPROPERTY(EditAnywhere, Category = Projectile)
-	FRotator HitDirection;
-
-	UPROPERTY(EditAnywhere, Category = Projectile)
-	float HitForce;
-
 	UPROPERTY(EditAnywhere)
 	class UStaticMeshComponent* StaticMeshComp;
 	
+	// 공 세팅시 정해지는 수치 (타구바에 따라 최종수치는 달라질수 있음)
+	UPROPERTY(EditAnywhere)
+	FVector ImpulseAmount; // 세기
+
+	UPROPERTY(EditAnywhere)
+	FVector TorqueAmount; // 
+	
+	// MAGNUS EFFECT
+	UPROPERTY(EditAnywhere)
+	float MagnusScalar = 0.01f;
+
+	// 공 치기!
 	UFUNCTION()
 	void Shot();
+
+	// VISUALIZATION
+	UPROPERTY(EditAnywhere)
+	float VisualizationCycle = 0.02f;
+	FTimerHandle VisualizationHandle;
+	FVector LastLocation;
+	void Visualize();
+	
+	// 시뮬레이션용
+	bool simulated = false;
+	
+	FTransform InitialTransform;
+	
+	UFUNCTION()
+	void ResetGolfBall();
 };
